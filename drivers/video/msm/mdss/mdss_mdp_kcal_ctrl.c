z2-dev/android_kernel_zuk_msm8996

mdss: Back to stock saturation default value on z2_row.

drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -561,7 +561,11 @@ static int kcal_ctrl_probe(struct platform_device *pdev)
 	lut_data->minimum = 0x23;
 	lut_data->invert = 0x0;
 	lut_data->hue = 0x0;
+#ifdef CONFIG_PRODUCT_Z2_PLUS
 	lut_data->sat = 0x113;
+#else
+	lut_data->sat = DEF_PA;
+#endif
 	lut_data->val = DEF_PA;
 	lut_data->cont = DEF_PA;
 

characteress2/overpowered

drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -561,7 +561,11 @@ static int kcal_ctrl_probe(struct platform_device *pdev)
 	lut_data->minimum = 0x23;
 	lut_data->invert = 0x0;
 	lut_data->hue = 0x0;
+#ifdef CONFIG_MACH_ZUK_Z2_PLUS
 	lut_data->sat = 0x113;
+#else
+	lut_data->sat = DEF_PA;
+#endif
 	lut_data->val = DEF_PA;
 	lut_data->cont = DEF_PA;
 